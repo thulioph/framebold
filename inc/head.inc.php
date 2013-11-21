e658950b88a8b16e9074c53c7e653d5651c52964
Thulio Philipe <thuthu__@hotmail.com>
2013-11-21 15:01:15 -0200

    inserindo html5shiv

--- inc/head.inc.php
+++ inc/head.inc.php
@@ -3,9 +3,10 @@
     <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
     <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
     <!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
-<html xmlns:og="http://ogp.me/ns#" lang="pt-BR"> 
+<html xmlns:og="http://ogp.me/ns#" lang="pt-BR">
     <head>
         <!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" /><![endif]-->
+        <!--[if lt IE 9]><script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 
         <meta charset="utf-8">
         <meta http-equiv="Content-Type" content="text/html;charset=utf-8" >

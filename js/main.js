e43ab4704b25208ab947721809e434d210904fd2
Thulio Philipe <thuthu__@hotmail.com>
2013-11-21 15:00:49 -0200

    inserindo function

--- js/main.js
+++ js/main.js
@@ -1,30 +1,34 @@
 head.js("js/vendor/jquery-1.10.2.min.js", "js/vendor/modernizr.js", "js/vendor/jquery.cycle.all.js",
  "js/vendor/colorbox.min.js", "js/vendor/mask_input.js", "js/vendor/jquery.lazyload.min.js", function(){
 
-	// Cycle - http://jquery.malsup.com/cycle/
-		// Algumas configurações do plugin
-		$("").cycle({
-			fx:     'fade',
-			speed:  'slow',
-			timeout: 6000,
-			next:   '#next',
-			prev:   '#prev',
-		});
+ 	jQuery(function() {
 
-	// Mask Input - http://digitalbush.com/projects/masked-input-plugin/
-		// Configuração para inputs de telefone
-		$("").mask("(99) 9999-9999");
+		// Cycle - http://jquery.malsup.com/cycle/
+			// Algumas configurações do plugin
+			$("").cycle({
+				fx:     'fade',
+				speed:  'slow',
+				timeout: 6000,
+				next:   '#next',
+				prev:   '#prev',
+			});
 
-	// Colorbox - http://www.jacklmoore.com/colorbox/
-		// Configuração padrão do plugin
-		$('a.gallery').colorbox({
-			rel:'gal'
-		});
+		// Mask Input - http://digitalbush.com/projects/masked-input-plugin/
+			// Configuração para inputs de telefone
+			$("").mask("(99) 9999-9999");
 
-	// Lazyload - http://www.appelsiini.net/projects/lazyload
-		// Adiciona uma class dinamicamente em todas as imagens
-		$("img").addClass("lazy");
-		// Executa o plugin
-		$("img.lazy").lazyload();
+		// Colorbox - http://www.jacklmoore.com/colorbox/
+			// Configuração padrão do plugin
+			$('a.gallery').colorbox({
+				rel:'gal'
+			});
+
+		// Lazyload - http://www.appelsiini.net/projects/lazyload
+			// Adiciona uma class dinamicamente em todas as imagens
+			$("img").addClass("lazy");
+			// Executa o plugin
+			$("img.lazy").lazyload();
+
+	});
 
 });
